36f09bf3476a41b4b0e338c84cb110e278948ce9
Vladislav <11332266vlados@gmail.com>
2022-05-03 19:49:48 +0300

    homework for lesson number 4

---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Телефон номер 1',
+    image: '/img/phone.jpg',
+    price: 26500,
+  },
+  {
+    title: 'Телефон номер 2',
+    image: '/img/phone-2.jpg',
+    price: 26700,
+  },
+  {
+    title: 'Телефон номер 3',
+    image: '/img/phone-3.jpg',
+    price: 22800,
+  },
+  {
+    title: 'Телефон номер 4',
+    image: '/img/phone-square.jpg',
+    price: 26890,
+  },
+  {
+    title: 'Телефон номер 5',
+    image: '/img/phone-square-1.jpg',
+    price: 35100,
+  },
+  {
+    title: 'Телефон номер 5',
+    image: '/img/phone-square-2.jpg',
+    price: 55000,
+  },
+  {
+    title: 'Телефон номер 6',
+    image: '/img/phone-square-3.jpg',
+    price: 79000,
+  },
+  {
+    title: 'Телефон номер 7',
+    image: '/img/phone-square-4.jpg',
+    price: 12400,
+  },
+  {
+    title: 'Радионяня',
+    image: '/img/radio.jpg',
+    price: 1500,
+  },
+  {
+    title: 'Доска на колёсах',
+    image: '/img/board.jpg',
+    price: 1999,
+  },
+  {
+    title: 'Велосипед для дрифта',
+    image: '/img/bicycle.jpg',
+    price: 70000,
+  },
+  {
+    title: 'Наушники',
+    image: '/img/airpods.jpg',
+    price: 5,
+  },
+];
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
-import HelloWorld from '@/helloWorld';
-import alertHelloWorld from '@/alertHelloWorld';
+// import HelloWorld from '@/helloWorld';
+// import alertHelloWorld from '@/alertHelloWorld';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 
-alertHelloWorld.soAlert(HelloWorld.hello);
-alertHelloWorld.soAlert(HelloWorld.world);
+// alertHelloWorld.soAlert(HelloWorld.hello);
+// alertHelloWorld.soAlert(HelloWorld.world);
 
 new Vue({
   render: (h) => h(App),
